2d95639bd60889ba3a571559fd7f635bf2079da5
goudardroberto <91286418+goudardroberto@users.noreply.github.com>
2022-01-05 14:03:07 -0300

    Add files via upload

---
+++ b6_conversor_Temperatura.py
@@ -0,0 +1,6 @@
+"""Programa que converte uma temperatura digitando em
+graus Celsius para graus Fahrenheit."""
+
+c = float(input("Digite uma temperatura em celsius: "))
+f = ((9 * c) / 5) + 32
+print(f"A temperatura em celsius: {c} terá o valor em fahrenheit: {f}.")
