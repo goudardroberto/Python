4a54f6ff47feba38a50c4926d5a16de95989b26a
goudardroberto <91286418+goudardroberto@users.noreply.github.com>
2022-01-05 20:52:26 -0300

    Update h3_saindo_de_loops_com_Break.py

--- h3_saindo_de_loops_com_Break.py
+++ h3_saindo_de_loops_com_Break.py
@@ -22,3 +22,23 @@ while True:
     if comando == "S":
         break
 """
+
+"""
+Crie um programa que leia números inteiros pelo teclado.
+O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada.
+No final, mostre quantos números foram digitados e qual foi a soma entre elas (desconsiderando o flag)."""
+
+contador = num = s = 0
+
+while True:
+    num = int(input("Digite um valor [999 para parar: "))
+    # Aqui ele não somará o 999 pois a condição esta antes da soma dos valores
+    if num == 999:
+        break
+    s += num
+    contador += 1
+# O .format() está desatualizado
+# print("A soma vale {}!".format(s))
+# Utilizar a formatação de strings como está abaixo
+print(f"Foram digitados {contador} e a soma vale {s}")
+
