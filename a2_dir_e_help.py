c81078d37fc35ecbd314cfb8aebc0be364936f06
goudardroberto <91286418+goudardroberto@users.noreply.github.com>
2022-01-05 12:16:00 -0300

    Add files via upload

---
+++ a2_dir_e_help.py
@@ -0,0 +1,17 @@
+"""
+Utilitários Python para auxiliar na programação.
+Podemos abrir o terminal e fazer as verificações.
+
+dir -> Apresenta todos os atributos/métodos disponíveis para determinado tipo de dado ou variável
+
+dir(tipo de dado/variável)
+
+dir("Roberto")
+
+help -> Apresenta a documentação/como utilizar os atributos/propriedades e funções/métodos disponíveis
+para determinado tipo de dado ou variável.
+
+help(tipo de dado/variável)
+
+help(Roberto.lower())
+"""
---
+++ a3_recebendo_dados_do_Usuario.py
@@ -0,0 +1,46 @@
+"""
+Recebendo dados do usuário
+
+input significa entrada
+
+Qualquer dado recebido pelo input é do tipo string
+
+Em Python, string é tudo que estiver entre:
+- Aspas simples;
+- Aspas duplas;
+- Aspas simples triplas;
+- Aspas duplas triplas;
+
+Cast é a conversão de um tipo de dado para outro
+- int(idade) => cast
+"""
+from datetime import date
+
+# Entrando com dados
+print("Qual o seu nome? ")
+nome = input()
+print(f"Seja bem-vindo(a) {nome}.", end="")
+
+# Tirando os espaços com .strip()
+# Passando para letra maiúscula com .upper()
+# Fatiando a primeira letra da palavra com [0]
+print(" Qual o seu sexo? ")
+sexo = input().strip().upper()[0]
+
+# Trazendo o dia atual e pegando o ano do resultado
+# Passando a entrada do usuário para inteiro e passando o cálculo para idade
+print("Quando você nasceu? ")
+data = date.today().year
+nascimento = int(input())
+idade = data - nascimento
+
+# Processando
+
+# Saída de dados
+# Criando uma condicional para separar o sexo e tratar possível erro
+if sexo in "M":
+    print(f"O {nome} tem {idade} anos.")
+elif sexo in "F":
+    print(f"A {nome} tem {idade} anos.")
+else:
+    print("Sexo incorreto!")
