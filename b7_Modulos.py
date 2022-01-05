389c636edf51e415b11414cd5ebd71a5085f4636
goudardroberto <91286418+goudardroberto@users.noreply.github.com>
2022-01-05 14:11:53 -0300

    Add files via upload

---
+++ b7_Modulos.py
@@ -0,0 +1,25 @@
+""" Utilizando uma biblioteca"""
+
+# Puxando toda biblioteca
+# import math
+# Puxando apenas uma função
+# from math import sqrt
+
+# Arredondar pra cima: math.ceil()
+# Arredondar pra baixo: math.floor()
+# Raiz: math.sqrt()
+
+# num = int(input("Digite um número: "))
+# raiz = math.sqrt(num)
+# print("A raiz de {} é {}.".format(num, math.ceil(raiz)))
+
+# Utilizando números aleatórios
+import random
+
+# Puxando número aleatório
+num = random.random()
+print(num)
+
+# Puxando número aleatório inteiro
+num = random.randint(1, 10)
+print(num)
