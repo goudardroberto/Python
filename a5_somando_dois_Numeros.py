1478d3a317b8aa3e305ecdf9819ceb693a801751
goudardroberto <91286418+goudardroberto@users.noreply.github.com>
2022-01-05 13:14:03 -0300

    Add files via upload

---
+++ a5_somando_dois_Numeros.py
@@ -0,0 +1,6 @@
+# Programa que lê dois números e mostra a soma entre eles
+
+num1 = int(input("Digite um valor: "))
+num2 = int(input("Digite outro valor: "))
+soma = num1 + num2
+print(f"A soma entre {num1} e {num2} é: {soma}")
