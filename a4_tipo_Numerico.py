dfeeebaeeb53bc7464138350481fec434e67eb5a
goudardroberto <91286418+goudardroberto@users.noreply.github.com>
2022-01-05 12:19:35 -0300

    Add files via upload

---
+++ a4_tipo_Numerico.py
@@ -0,0 +1,40 @@
+"""
+Tipo numérico => -1; 0; 1
+
+Para consultar o tipo da variável, usamos o type() e passamos a variável como parâmetro
+
+Soma = 1 + 1
+Subtração = 1 - 1
+Multiplicação = 1 * 1
+Divisão = 1 / 1
+Resto = 1 % 1
+Quadrado = 2 ** 2
+Cubo = 2 ** 3
+
+
+# Exemplos de operações
+# num = 2
+# num = num + 2
+num = 4
+
+# Também podemos escrever assim:
+# num = 2
+# num += 2
+num = 4
+
+# num = 2
+# num -= 2
+num = 0
+
+# num = 2
+# num *= 2
+num = 4
+
+# num = 2
+# num /= 2
+num = 1.0
+
+# num 2
+# num //= 2
+num = 1
+"""
