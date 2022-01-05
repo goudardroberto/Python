f0fb9e2388f09fc23721fab461aac1d702b79f53
goudardroberto <91286418+goudardroberto@users.noreply.github.com>
2022-01-05 13:23:37 -0300

    Add files via upload

---
+++ a7_antecessor_Sucessor.py
@@ -0,0 +1,11 @@
+""" programa que lê um número Inteiro e mostre na tela o seu antecessor e seu sucessor."""
+
+# Guardando na variável
+# res = int(input("Digite um número: "))
+# ante = res - 1
+# suce = res + 1
+# print("O número inserido é: {}, seu antecessor é: {}, seu sucessor é: {}.".format(res, ante, suce))
+
+# Sem guardar o resultado na variável
+res = int(input("Digite um número: "))
+print(f"O número inserido é: {res}, seu antecessor é: {(res - 1)}, seu sucessor é: {(res + 1)}.")
