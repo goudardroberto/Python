153a72ccc64f0521923f746891736f6cae0521d7
goudardroberto <91286418+goudardroberto@users.noreply.github.com>
2022-01-05 21:04:41 -0300

    Update h2_loop_While.py

--- h2_loop_While.py
+++ h2_loop_While.py
@@ -31,3 +31,56 @@ resposta = ""
 while resposta != "S":
     resposta = input("Já acabou Jéssica? ").strip().upper()[0]
 """
+
+# Exemplo 3
+# for i in range(1, 10):
+#     print(i)
+# print("Fim!")
+
+# Criar uma variável contador para ter controle do looping
+# i = 1
+# while i < 10:
+#     print(i)
+      # Incrementar a variável para ter um fim
+#     i += 1
+# print("Fim!")
+
+# Exemplo 4
+# for i in range(1, 5):
+#     n = input("Entre com um valor: ")
+# print("Fim!")
+
+# i = 1
+# while i < 5:
+#     n = input("Entre com um valor: ")
+#     i += 1
+# print("Fim!")
+
+# Exemplo 5
+# Também podemos inserir a condição para a saída do looping ao digitar 0
+# i = 1
+# while i != 0:
+      # Aqui precisamos passar a variável para inteiro
+#     i = int(input("Entre com um número: "))
+# print("Fim!")
+
+# Exemplo 6
+# Podemos separar a condição para saída em outra variável
+# i = "S"
+# while i == "S":
+#     n = int(input("Entre com um valor: "))
+#     i = str(input("Quer continuar [ S/N ]: ")).upper()
+# print("Fim!")
+
+# Exemplo 7
+# Quando amarrar o 0 para saída do looping, sempre criar condição para não contabilizar ele
+n = 1
+par = impar = 0
+while n != 0:
+    n = int(input("Entre com um valor: "))
+    if n != 0:
+        if n % 2 == 0:
+            par += 1
+        else:
+            impar += 1
+print("Você digitou {} números pares e {} números ímpares!".format(par, impar))
