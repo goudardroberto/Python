c89ddce88deae05aa273eb54a8c619b6f7f42237
goudardroberto <91286418+goudardroberto@users.noreply.github.com>
2022-01-06 10:51:25 -0300

    Update j7_Listas.py

--- j7_Listas.py
+++ j7_Listas.py
@@ -335,3 +335,68 @@ cores = ['verde', 'amarelo', 'azul', 'branco']
 numeros = [5, 6, 7, 8, 9, 10]
 
 lista8 = [1, 2, 3]
+
+"""
+Listas de listas
+
+# Criando uma lista com outra dentro
+teste = list()
+teste.append('Gustavo')
+teste.append(40)
+galera = list()
+galera.append(teste)
+print(galera)
+
+# SEM FAZER CÓPIA - Ao adicionar elementos dentro de listas já criadas, todos os elementos ficam iguais
+teste = list()
+teste.append('Gustavo')
+teste.append(40)
+galera = list()
+galera.append(teste)
+teste[0] = 'Maria'
+teste[1] = 22
+galera.append(teste)
+print(galera)
+
+# COM A CÓPIA - Ao adicionar elementos dentro de listas já criadas, não temos elementos replicados
+teste = list()
+teste.append('Gustavo')
+teste.append(40)
+galera = list()
+galera.append(teste[:])
+teste[0] = 'Maria'
+teste[1] = 22
+galera.append(teste[:])
+print(galera)
+
+# Podemos retornar um elemento de uma lista dentro de outra lista
+galera = [['João', 19], ['Ana', 33], ['Joaquim', 13], ['Maria', 45]]
+print(galera[0])
+print(galera[2][1])
+for p in galera:
+    print(f'{p[0]} tem {p[1]} anos de idade.')
+"""
+
+# Pegando dados para adicionar a lista
+galera = list()
+dado = list()
+totmai = totmen = 0
+
+
+for c in range(0, 3):
+    dado.append(str(input('Nome: ')))
+    dado.append(int(input('Idade: ')))
+    galera.append(dado[:])
+    dado.clear()
+
+print(galera)
+
+for p in galera:
+    if p[1] >= 21:
+        print(f'{p[0]} é maior de idade.')
+        totmai += 1
+    else:
+        print(f'{p[0]} é menor de idade.')
+        totmen += 1
+
+print(f'Temos {totmai} maiores e {totmen} menores de idade.')
